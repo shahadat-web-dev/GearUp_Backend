059fdd51f597128ad4193804f1d9accd8848dbfb
shahadat-web-dev <155247969+shahadat-web-dev@users.noreply.github.com>
2026-07-17 23:40:26 +0600

    use middler ware app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import config from "./config";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.APP_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 app.get("/", (req: Request, res:Response) => {
    res.send("Hello , world!");
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT || 5000;
+const PORT = config.PORT;
 
 async function main(){
   try {
+    await prisma.$connect();
+    console.log("connected to the database successfully.");
+    
     app.listen(PORT, ()=>{
       console.log(`Server is runing on port ${PORT}`); 
     });
   } catch (error) {
     console.error("Error starting the server", error);
+    await prisma.$disconnect();
     process.exit(1);
   }
 
